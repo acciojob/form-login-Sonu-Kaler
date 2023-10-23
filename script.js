2abe13b541d644cb47fcd5d954a804350cd53eab
Sonu-Kaler <130778161+Sonu-Kaler@users.noreply.github.com>
2023-10-23 15:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 function getFormvalue() {
-    //Write your code here
-	
-  var x=document.getElementById("form1");
-  var s="";
-  for (var i=0;i<x.length;i++)
-  {
-   if (x.elements[i].value!='Submit')
-    {  
-      s+=x.elements[i].value;
-      s+=" ";
-     }  
-   }
-   alert(s);
-
+  alert('First Name: ' + firstName + 'Last Name: ' + lastName);
 }
